6e21d3df50cd056f18f882313ead3a338a6091a5
fiskz <root@fiskz.com>
2016-07-04 15:45:45 +0800

    add demo

--- TextColorFilterTool/LXTextColorFilter.h
+++ TextColorFilterTool/LXTextColorFilter.h
@@ -1,13 +1,27 @@
 //
 //  LXTextColorFilter.h
-//  colorBluetoothLampV3
+//  FiskzUILabelColorFilterTool
 //
-//  Created by wgz on 16/6/25.
-//  Copyright © 2016年 TRP. All rights reserved.
+//  Created by wgz on 16/7/4.
+//  Copyright © 2016年 fiskz. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-
+#import <UIKit/UIKit.h>
 @interface LXTextColorFilter : NSObject
-+(NSMutableAttributedString *) chengesColorText:(NSString*) butedString;
+
+@property (nonatomic, strong) UIColor* normalColor;
+
+@property (nonatomic, strong) UIColor* selectedColor;
+
++ (LXTextColorFilter *)sharedLXTextColorFilter;
+
+-(void) setStateNormalColor:(UIColor*) normal StateSelected:(UIColor*)selected;
+
+-(NSMutableAttributedString *) chengesColorText:(NSString*) butedString;
+
+-(UILabel *) chengesColorText:(NSString*) butedString AndUILabel:(UILabel*) control;
+
+-(UIButton *) chengesColorText:(NSString*) butedString AndUIButton:(UIButton*) control forState:(UIControlState) state;
+
 @end
--- TextColorFilterTool/LXTextColorFilter.m
+++ TextColorFilterTool/LXTextColorFilter.m
@@ -1,30 +1,69 @@
 //
 //  LXTextColorFilter.m
-//  colorBluetoothLampV3
+//  FiskzUILabelColorFilterTool
 //
-//  Created by wgz on 16/6/25.
-//  Copyright © 2016年 TRP. All rights reserved.
+//  Created by wgz on 16/7/4.
+//  Copyright © 2016年 fiskz. All rights reserved.
 //
 
 #import "LXTextColorFilter.h"
-
+//获取十六进制颜色值
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 @implementation LXTextColorFilter
 
-
-+(NSMutableAttributedString *) chengesColorText:(NSString*) butedString
++ (LXTextColorFilter *)sharedLXTextColorFilter
 {
-    NSArray *array  = [butedString componentsSeparatedByString:@"["];
-    NSString * str1 = array[0];
-    NSString * str2 = array[1];
-    NSArray *array2 = [str2 componentsSeparatedByString:@"]"];
-    NSString * str3 = array2[0];
-    NSString * str4 = array2[1];
+    static LXTextColorFilter *sharedLXTextColorFilterInstance = nil;
+    static dispatch_once_t predicate;
+    dispatch_once(&predicate, ^{
+        sharedLXTextColorFilterInstance = [[self alloc] init];
+    });
+    return sharedLXTextColorFilterInstance;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.normalColor = UIColorFromRGB(0x6a6a6a);
+        self.selectedColor = UIColorFromRGB(0x00b1ff);
+    }
+    return self;
+}
+
+-(void) setStateNormalColor:(UIColor*) normal StateSelected:(UIColor*)selected
+{
+    self.normalColor = normal;
+    self.selectedColor = selected;
+}
+
+-(NSMutableAttributedString *) chengesColorText:(NSString*) butedString
+{
+    NSArray * one  = [butedString componentsSeparatedByString:@"["];
+    NSString * str1 = one[0];
+    NSString * str2 = one[1];
+    NSArray * two = [str2 componentsSeparatedByString:@"]"];
+    NSString * str3 = two[0];
+    NSString * str4 = two[1];
     NSString * str = [str1 stringByAppendingString:[str3 stringByAppendingString:str4]];
     NSMutableAttributedString *attributedStr = [[NSMutableAttributedString alloc] initWithString: str];
-    //分段控制，最开始4个字符颜色设置成黑色
-    [attributedStr addAttribute: NSForegroundColorAttributeName value: UIColorFromRGB(0x6a6a6a) range: NSMakeRange(0,str.length)];
-   
-    [attributedStr addAttribute: NSForegroundColorAttributeName value: UIColorFromRGB(0x00b1ff) range: NSMakeRange(str1.length,str3.length)];
+    //分段控制，颜色变化区域
+    [attributedStr addAttribute: NSForegroundColorAttributeName value: self.normalColor range: NSMakeRange(0,str.length)];
+    
+    [attributedStr addAttribute: NSForegroundColorAttributeName value:self.selectedColor range: NSMakeRange(str1.length,str3.length)];
     return attributedStr;
 }
+
+-(UILabel *) chengesColorText:(NSString*) butedString AndUILabel:(UILabel*) control
+{
+    control.attributedText = [self chengesColorText:(NSString*) butedString];
+    return control;
+}
+
+-(UIButton *) chengesColorText:(NSString*) butedString AndUIButton:(UIButton*) control forState:(UIControlState) state
+{
+    [control setAttributedTitle:[self chengesColorText:(NSString*) butedString] forState:state];
+    return control;
+}
+
 @end
